e0418e4068454ed1078f6ff38969e6af88100581
Andres <andresrlaguna@gmail.com>
2024-02-02 18:07:49 -0500

    commit con nuevo comando 1

--- curso.js
+++ curso.js
@@ -1,35 +1,6 @@
 //Tipo de dato primitimo - Inmutable
 
-let primeraVariable = 5
-primeraVariable = primeraVariable + 15
-console.log(primeraVariable)
-
-let esVerdadero = true
-console.log(esVerdadero)
-
-//Tipo de dato complejo - Mutable
-
-let usuarioPrimeraVariable = {nombre : "Andres Rdriguez" , edad : "15"}
-usuarioPrimeraVariable.edad = 29
-console.log(usuarioPrimeraVariable)
-
-let frutasColombiana = ["chulupa", "pera", "uvas", "banano"]
-frutasColombiana [2] = "fresas"
-console.log(frutasColombiana)
-
-function cambiarNombre (cualquiercosa){
-    cualquiercosa.nombre = "Andres"
-}
-function cambiarEdad (cualquiercosa){
-    cualquiercosa.edad = "100"
-}
-
-let personaReal = { nombre : "personaje1", edad : "55 años"}
-
-cambiarNombre(personaReal)
-cambiarEdad(personaReal)
-
-console.log(personaReal)
-
-const suma = '5' + 15
-console.log(suma)
+let votosPetro = 11294986
+let votosRodolfo = 10604337
+diferenciaDeVotos = votosPetro - votosRodolfo
+console.log (diferenciaDeVotos)
